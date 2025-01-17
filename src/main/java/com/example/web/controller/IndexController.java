ee7a871647aae9a83d990cc89cf44e30cee24a0b
juheonLee <jh.lee1@altimedia.com>
2025-01-17 13:56:23 +0900

    Added prefix and suffix beans

--- src/main/java/com/example/web/controller/IndexController.java
+++ src/main/java/com/example/web/controller/IndexController.java
@@ -10,10 +10,10 @@ public class IndexController implements Controller{
 
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        ModelAndView mv = new ModelAndView();
+        ModelAndView mv = new ModelAndView("index");
         System.out.println("Checking Index Controller");
         mv.addObject("data", "Hello Spring MVC!");
-        mv.setViewName("/WEB-INF/view/index.jsp");
+//        mv.setViewName("/WEB-INF/view/index.jsp");
         //만약 "WEB-INF/view/index.jsp 로 상대경로를 하면 dispatcher-servlet.xml의 bean id를 기준으로 찾아감
         // bean id="/aa/index" 일 때 http://.../aa/index 로 접속 가능하고 view도 "aa/WEB-INF/view/index.jsp"를 찾으려고 시도
         return mv;
